469e6fb0529331aafdcb59cdb6ef02643fe2d349
weihao_ling <1020529941@qq.com>
2019-05-27 19:37:15 +0800

    remote:添加分享代码

---
+++ javascript技术分享/code/code1.js
@@ -0,0 +1,28 @@
+// 4
+var a = "string"
+a.name = "张三"
+console.log(a.name)
+
+var b = ["a","b"]
+b.name = "李四"
+console.log(b.name)
+console.log(b)
+// 5.1
+var a = "abc";
+console.log(typeof a); // "string"
+var b = 123;
+console.log(typeof b); // "number"
+var c = true;
+console.log(typeof c); // "boolean"
+var d = null;
+console.log(typeof d); // "object"
+var f = undefined;
+console.log(typeof f); // "undefined"
+var g;
+console.log(typeof g); // "undefined"
+console.log(typeof x); // "undefined"
+// 5.2
+var a = []
+console.log(a instanceof Array) // trye
+var b = {}
+console.log(b instanceof Object) // true
