39756ab3ff64ef6e1a0b266cfde33c8144916f32
lingweihao <1020529941@qq.com>
2019-05-31 00:34:31 +0800

    代码提交

--- javascript技术分享/code/code3.js
+++ javascript技术分享/code/code3.js
@@ -1,3 +1,4 @@
+// 变量提升 https://www.cnblogs.com/guanhuachen/p/6038957.html
 fn()
 function fn(){
     console.log("声明")
@@ -6,3 +7,49 @@ fn1()
 var fn1 = function(){
     console.log("表达式")
 }
+
+
+// js会把变量的声明提升到上面
+var a;
+console.log(a);
+a='hello world';
+
+// 上下代码相等
+
+
+var name = "world";
+(function () {
+if (typeof name == 'undefined') {
+var name = 'yang';
+console.log('Hello ' + name)
+} else {
+console.log('Hello ' + name)
+}
+})()
+
+var name = "world";
+(function () {
+var name;
+if (typeof name == 'undefined') {
+var name = 'yang';
+console.log('Hello ' + name)
+} else {
+console.log('Hello ' + name)
+}
+})()
+
+// 使用let关键字来避免变量提升
+var name = "world";
+(function () {
+if (typeof name == 'undefined') {
+let name = 'yang';
+
+console.log('Hello ' + name)
+} else {
+console.log('Hello ' + name)
+}
+})()
+
+// 闭包 https://www.cnblogs.com/duanlianjiang/p/5036671.html
+
+
